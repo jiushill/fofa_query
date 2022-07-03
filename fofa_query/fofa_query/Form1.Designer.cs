8a4a5b9aab45309398612733c4fd495b6c8e1b3b
九世 <422926799@qq.com>
2022-07-04 03:58:44 +0800

    Add files via upload

---
+++ fofa_query/fofa_query/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace fofa_query
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cookiebox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pagebox = new System.Windows.Forms.TextBox();
+            this.searchbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.querybox = new System.Windows.Forms.Button();
+            this.searchcurl = new System.Windows.Forms.TextBox();
+            this.urllabel = new System.Windows.Forms.Label();
+            this.title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.url = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ip = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.resultlistview = new System.Windows.Forms.ListView();
+            this.port = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nation = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.area = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.errortext = new System.Windows.Forms.Label();
+            this.timesleep = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.importbutton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.savelist = new System.Windows.Forms.CheckedListBox();
+            this.clearbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cookiebox
+            // 
+            this.cookiebox.Location = new System.Drawing.Point(74, 29);
+            this.cookiebox.Name = "cookiebox";
+            this.cookiebox.Size = new System.Drawing.Size(348, 21);
+            this.cookiebox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip2.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip2_Opening);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "cookie";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "page";
+            // 
+            // pagebox
+            // 
+            this.pagebox.Location = new System.Drawing.Point(74, 105);
+            this.pagebox.Name = "pagebox";
+            this.pagebox.Size = new System.Drawing.Size(63, 21);
+            this.pagebox.TabIndex = 5;
+            this.pagebox.Text = "2";
+            // 
+            // searchbox
+            // 
+            this.searchbox.Location = new System.Drawing.Point(74, 56);
+            this.searchbox.Name = "searchbox";
+            this.searchbox.Size = new System.Drawing.Size(348, 21);
+            this.searchbox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "search";
+            // 
+            // querybox
+            // 
+            this.querybox.Location = new System.Drawing.Point(194, 105);
+            this.querybox.Name = "querybox";
+            this.querybox.Size = new System.Drawing.Size(75, 23);
+            this.querybox.TabIndex = 8;
+            this.querybox.Text = "query";
+            this.querybox.UseVisualStyleBackColor = true;
+            this.querybox.Click += new System.EventHandler(this.querybox_Click);
+            // 
+            // searchcurl
+            // 
+            this.searchcurl.Location = new System.Drawing.Point(74, 2);
+            this.searchcurl.Name = "searchcurl";
+            this.searchcurl.Size = new System.Drawing.Size(406, 21);
+            this.searchcurl.TabIndex = 10;
+            this.searchcurl.Text = "https://fofa.info/result?qbase64={0}&page={1}&page_size=10";
+            // 
+            // urllabel
+            // 
+            this.urllabel.AutoSize = true;
+            this.urllabel.Location = new System.Drawing.Point(12, 9);
+            this.urllabel.Name = "urllabel";
+            this.urllabel.Size = new System.Drawing.Size(23, 12);
+            this.urllabel.TabIndex = 11;
+            this.urllabel.Text = "url";
+            // 
+            // title
+            // 
+            this.title.Text = "title";
+            this.title.Width = 122;
+            // 
+            // url
+            // 
+            this.url.Text = "url";
+            this.url.Width = 158;
+            // 
+            // ip
+            // 
+            this.ip.Text = "ip";
+            this.ip.Width = 106;
+            // 
+            // resultlistview
+            // 
+            this.resultlistview.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.title,
+            this.url,
+            this.ip,
+            this.port,
+            this.nation,
+            this.area});
+            this.resultlistview.FullRowSelect = true;
+            this.resultlistview.GridLines = true;
+            this.resultlistview.Location = new System.Drawing.Point(12, 168);
+            this.resultlistview.Name = "resultlistview";
+            this.resultlistview.Size = new System.Drawing.Size(638, 288);
+            this.resultlistview.TabIndex = 9;
+            this.resultlistview.UseCompatibleStateImageBehavior = false;
+            this.resultlistview.View = System.Windows.Forms.View.Details;
+            // 
+            // port
+            // 
+            this.port.Text = "port";
+            this.port.Width = 75;
+            // 
+            // nation
+            // 
+            this.nation.Text = "nation";
+            this.nation.Width = 115;
+            // 
+            // area
+            // 
+            this.area.Text = "area";
+            this.area.Width = 173;
+            // 
+            // errortext
+            // 
+            this.errortext.AutoSize = true;
+            this.errortext.Location = new System.Drawing.Point(14, 463);
+            this.errortext.Name = "errortext";
+            this.errortext.Size = new System.Drawing.Size(89, 12);
+            this.errortext.TabIndex = 12;
+            this.errortext.Text = "异常捕获输出：";
+            // 
+            // timesleep
+            // 
+            this.timesleep.FormattingEnabled = true;
+            this.timesleep.Items.AddRange(new object[] {
+            "1000",
+            "5000",
+            "10000",
+            "15000",
+            "20000"});
+            this.timesleep.Location = new System.Drawing.Point(596, 1);
+            this.timesleep.Name = "timesleep";
+            this.timesleep.Size = new System.Drawing.Size(102, 20);
+            this.timesleep.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(501, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "延时设置(毫秒)";
+            // 
+            // savebutton
+            // 
+            this.savebutton.Location = new System.Drawing.Point(306, 105);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(82, 23);
+            this.savebutton.TabIndex = 16;
+            this.savebutton.Text = "save";
+            this.savebutton.UseVisualStyleBackColor = true;
+            this.savebutton.Click += new System.EventHandler(this.savebutton_Click);
+            // 
+            // importbutton
+            // 
+            this.importbutton.Location = new System.Drawing.Point(405, 105);
+            this.importbutton.Name = "importbutton";
+            this.importbutton.Size = new System.Drawing.Size(75, 23);
+            this.importbutton.TabIndex = 17;
+            this.importbutton.Text = "import";
+            this.importbutton.UseVisualStyleBackColor = true;
+            this.importbutton.Click += new System.EventHandler(this.importbutton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(501, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "保存选项";
+            // 
+            // savelist
+            // 
+            this.savelist.FormattingEnabled = true;
+            this.savelist.Items.AddRange(new object[] {
+            "title",
+            "url",
+            "ip",
+            "port",
+            "nation",
+            "area"});
+            this.savelist.Location = new System.Drawing.Point(596, 38);
+            this.savelist.Name = "savelist";
+            this.savelist.Size = new System.Drawing.Size(90, 100);
+            this.savelist.TabIndex = 19;
+            // 
+            // clearbutton
+            // 
+            this.clearbutton.Location = new System.Drawing.Point(503, 105);
+            this.clearbutton.Name = "clearbutton";
+            this.clearbutton.Size = new System.Drawing.Size(78, 23);
+            this.clearbutton.TabIndex = 20;
+            this.clearbutton.Text = "clear";
+            this.clearbutton.UseVisualStyleBackColor = true;
+            this.clearbutton.Click += new System.EventHandler(this.clearbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 488);
+            this.Controls.Add(this.clearbutton);
+            this.Controls.Add(this.savelist);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.importbutton);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.timesleep);
+            this.Controls.Add(this.errortext);
+            this.Controls.Add(this.urllabel);
+            this.Controls.Add(this.searchcurl);
+            this.Controls.Add(this.resultlistview);
+            this.Controls.Add(this.querybox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.searchbox);
+            this.Controls.Add(this.pagebox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cookiebox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cookiebox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pagebox;
+        private System.Windows.Forms.TextBox searchbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button querybox;
+        private System.Windows.Forms.TextBox searchcurl;
+        private System.Windows.Forms.Label urllabel;
+        private System.Windows.Forms.ColumnHeader title;
+        private System.Windows.Forms.ColumnHeader url;
+        private System.Windows.Forms.ColumnHeader ip;
+        private System.Windows.Forms.ListView resultlistview;
+        private System.Windows.Forms.ColumnHeader port;
+        private System.Windows.Forms.ColumnHeader nation;
+        private System.Windows.Forms.ColumnHeader area;
+        private System.Windows.Forms.Label errortext;
+        private System.Windows.Forms.ComboBox timesleep;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button savebutton;
+        private System.Windows.Forms.Button importbutton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckedListBox savelist;
+        private System.Windows.Forms.Button clearbutton;
+    }
+}
+
---
+++ fofa_query/fofa_query/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace fofa_query
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 form1;
+        public static string searchurl;
+        public static string searchtext;
+        public static string cookies;
+        public static int page;
+        public static int sleeptime;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timesleep.Text=timesleep.Items[2].ToString();
+            sleeptime = Int32.Parse(timesleep.Text);
+            savebutton.Enabled = false;
+            for (int calc = 0; calc < savelist.Items.Count; calc++) {
+                savelist.SetItemChecked(calc,true);
+            }
+            form1 = this;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void contextMenuStrip2_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void buttonstop(int id)
+        {
+            if (id == 0)
+            {
+                fofa_query.Form1.form1.querybox.Invoke(new Action(() =>
+                {
+                    fofa_query.Form1.form1.querybox.Enabled = false;
+                }));
+            }
+            else {
+                fofa_query.Form1.form1.querybox.Invoke(new Action(() =>
+                {
+                    fofa_query.Form1.form1.querybox.Enabled = true;
+                }));
+
+            }
+        }
+
+        private void querybox_Click(object sender, EventArgs e)
+        {
+            int errorid = 0;
+            page=0;
+            searchurl = searchcurl.Text;
+            searchtext = Convert.ToBase64String(Encoding.UTF8.GetBytes(searchbox.Text)).Replace("=","%3D");
+            cookies = cookiebox.Text;
+            try
+            {
+                page =int.Parse(pagebox.Text);
+            }
+            catch {
+                errorid = 1;
+                MessageBox.Show("page不是数字");
+            }
+
+            if (errorid != 1)
+            {
+                ThreadStart thStat = new ThreadStart(fofa_search.start);
+                Thread thread = new Thread(thStat);
+                thread.Priority = ThreadPriority.Highest;
+                thread.IsBackground = true;
+                thread.Start();
+                
+            }
+        }
+
+
+        private void savebutton_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            List<string> tmparray = new List<string>();
+            var savelist = fofa_query.Form1.form1.savelist.Items;
+            for (int tmp = 0; tmp < savelist.Count; tmp++) {
+                if (fofa_query.Form1.form1.savelist.GetItemChecked(tmp)) {
+                    tmparray.Add(savelist[tmp].ToString());
+                }
+            }
+
+            var resultlist = fofa_query.Form1.form1.resultlistview.Items;
+            for (int calc = 0; calc < resultlist.Count; calc++) {
+                string title = resultlist[calc].SubItems[0].Text;
+                string url = resultlist[calc].SubItems[1].Text;
+                string ip = resultlist[calc].SubItems[2].Text;
+                string port = resultlist[calc].SubItems[3].Text;
+                string nation = resultlist[calc].SubItems[4].Text;
+                string area = resultlist[calc].SubItems[5].Text;
+                if (tmparray.Contains("title")) {
+                    result += title;
+                }
+
+                if (tmparray.Contains("url")) {
+                    result +=" "+url;
+                }
+
+                if (tmparray.Contains("ip")) {
+                    result += " "+ip;
+                }
+
+                if (tmparray.Contains("port")) {
+                    result +=" "+port;
+                }
+
+                if (tmparray.Contains("nation")) {
+                    result += " " + nation;
+                }
+
+                if (tmparray.Contains("area")) {
+                    result += " " + area;
+                }
+                result += "\n";
+            }
+            File.WriteAllText("save.txt", result);
+            MessageBox.Show("结果已保存到save.txt");
+        }
+
+        private void importbutton_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("config.txt"))
+            {
+                string[] config = File.ReadAllText("config.txt").Split('\n');
+                try
+                {
+                    string url = config[0].Replace("[url]=","").Replace("\r","");
+                    string cookie = config[1].Replace("[cookie]=", "").Replace("\r", "");
+                    string search = config[2].Replace("[search]=", "").Replace("\r", "");
+                    searchcurl.Text = url;
+                    cookiebox.Text = cookie;
+                    searchbox.Text = search;
+                }
+                catch {
+                    MessageBox.Show("config.txt格式不正确") ;
+                }
+            }
+            else {
+                MessageBox.Show("当前目录不存在config.txt");
+            }
+        }
+
+        private void clearbutton_Click(object sender, EventArgs e)
+        {
+            resultlistview.Items.Clear();
+        }
+        public class fofa_search
+        {
+
+            public static void start()
+            {
+                fofa_query.Form1.buttonstop(0);
+                string cookie = fofa_query.Form1.cookies;
+                string url = fofa_query.Form1.searchurl;
+                int page = fofa_query.Form1.page;
+                string search = fofa_query.Form1.searchtext;
+                int timesleep = fofa_query.Form1.sleeptime;
+                fofa_query.Form1.form1.timesleep.Invoke(new Action(() => {
+                   timesleep=Int32.Parse(fofa_query.Form1.form1.timesleep.Text);
+                }));
+                try
+                {
+                    for (int calc = 1; calc < page+1; calc++)
+                    {
+                        string url2 = string.Format(url, searchtext, calc.ToString()); //要爬取的url生成
+                        Debug.Print(url2);
+                        HttpClientHandler header = new HttpClientHandler() { UseCookies = false };
+                        HttpClient request = new HttpClient(header);
+                        HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, url2);
+                        message.Headers.Add("KeepAlive", "true");
+                        message.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36");
+                        message.Headers.Add("Accept", "*/*");
+                        message.Headers.Add("Cookie", cookie);
+                        var task = Task.Run(async () =>
+                       {
+                           Thread.Sleep(timesleep);
+                           var result = await request.SendAsync(message);
+                           if (result.StatusCode.GetHashCode() == 406)
+                           {
+                               fofa_query.Form1.form1.errortext.Invoke(new Action(() =>
+                               {
+                                   fofa_query.Form1.form1.errortext.Text = "捕获异常：请求过快状态码406";
+                               }));
+                           }
+                           else {
+                               fofa_query.Form1.form1.errortext.Invoke(new Action(() =>
+                               {
+                                   fofa_query.Form1.form1.errortext.Text = "捕获异常：无异常";
+                               }));
+                           }
+                           string content = await result.Content.ReadAsStringAsync();
+                         //  Debug.Print(content);
+                           HtmlAgilityPack.HtmlDocument html = new HtmlAgilityPack.HtmlDocument();
+                           html.LoadHtml(content);
+                           HtmlNode layout = html.DocumentNode.SelectSingleNode("//*[@id='__layout']");
+                           HtmlNodeCollection titlelist = layout.SelectNodes("//div[@class=\"contentMain\"]/div[1]/p[1]");
+                           if (titlelist == null)
+                           {
+                               Debug.Print("titlelist is null");
+                               fofa_query.Form1.buttonstop(1);
+                           }
+                           HtmlNodeCollection iplist = layout.SelectNodes("//div[@class=\"contentMain\"]/div[@class]/p[2]/a");
+                           if (iplist == null)
+                           {
+                               Debug.Print("iplist is null");
+                               fofa_query.Form1.buttonstop(1);
+                           }
+                           HtmlNodeCollection urllist = layout.SelectNodes("//span[@class=\"aSpan\"]/a[1]");
+                           if (urllist == null)
+                           {
+                               Debug.Print("urllist is null");
+                               fofa_query.Form1.buttonstop(1);
+                           }
+                           HtmlNodeCollection portlist = layout.SelectNodes("//a[@class=\"portHover\"]");
+                           if (portlist == null)
+                           {
+                               Debug.Print("prolist is null");
+                               fofa_query.Form1.buttonstop(1);
+                           }
+                           HtmlNodeCollection nationlist = layout.SelectNodes("//a[@href and @class]");
+                           if (nationlist == null)
+                           {
+                               Debug.Print("nationlist is null");
+                               fofa_query.Form1.buttonstop(1);
+                           }
+                           HtmlNodeCollection arealist = layout.SelectNodes("//div[@class=\"contentLeft\"]/p[5]/a[1]");
+                           if (arealist == null)
+                           {
+                               Debug.Print("arealist is null");
+                               fofa_query.Form1.buttonstop(1);
+                           }
+                           Debug.Print(string.Format("title:{0} ip:{1} url:{2} port:{3} nation:{4}", titlelist.Count.ToString(), iplist.Count.ToString(), urllist.Count.ToString(), portlist.Count.ToString(), nationlist.Count.ToString()));
+                           for (int tmpcalc = 0; tmpcalc < titlelist.Count; tmpcalc++)
+                           {
+                               ListViewItem item = new ListViewItem();
+                               item.Text = titlelist[tmpcalc].InnerText.Replace("\n","");
+                               item.SubItems.Add(urllist[tmpcalc].InnerText.Replace("\n", ""));
+                               item.SubItems.Add(iplist[tmpcalc].InnerText.Replace("\n", ""));
+                               item.SubItems.Add(portlist[tmpcalc].InnerText.Replace("\n", ""));
+                               item.SubItems.Add(nationlist[tmpcalc].InnerText.Replace("\n", ""));
+                               item.SubItems.Add(arealist[tmpcalc].InnerText.Replace("\n", ""));
+                               fofa_query.Form1.form1.resultlistview.Invoke(new Action(() =>
+                               {
+                                   fofa_query.Form1.form1.resultlistview.Items.Add(item);
+                               }));
+                           }
+                           fofa_query.Form1.buttonstop(1);
+
+                       });
+                    }
+                    fofa_query.Form1.form1.resultlistview.Invoke(new Action(() =>
+                    {
+                        fofa_query.Form1.form1.savebutton.Enabled = true;
+                    }));
+                }
+                catch(Exception error) {
+                    fofa_query.Form1.form1.errortext.Invoke(new Action(() =>
+                    {
+                        fofa_query.Form1.form1.errortext.Text = error.Source;
+                    }));
+                }
+            }
+        }
+
+    }
+}
